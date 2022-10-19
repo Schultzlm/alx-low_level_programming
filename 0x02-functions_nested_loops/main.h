1dd974a1e580286c8e0c2428628953ee2d7431a6
Schultzlm <litokwas@programmer.net>
2022-10-19 10:26:52 -0700

    0_putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print _alphabet (void);
+void print _alphabet(void);
 void print_alphabet_x10(void);
 int _is lower(int c);
 int _isalpha(int c);
@@ -12,6 +12,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _pucc\har(char.c);
+int _putchar(char c);
 
 #endif
