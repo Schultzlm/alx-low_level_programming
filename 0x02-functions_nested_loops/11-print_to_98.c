37e4b86d9f5bda936c86284d090c48ec9c5519cc
Schultzlm <litokwas@programmer.net>
2022-10-21 02:00:03 -0700

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,17 @@
  */
 void print_to_98(int n)
 {
-	int i;
 
-	if (n > 98)
+	if (n >= 98)
 	{
-		for (i = n; i > 97; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(",");
-			}
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 	else
 	{
-		for (i = n; i < 99; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-			{
-				printf(",");
-			}
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-	printf("\n");
 }
