56397f1bae64283f48181daf2ce711029132d98e
Schultzlm <litokwas@programmer.net>
2022-10-24 10:24:59 -0700

    A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string _ this will Reverse a string
+ * @s: the string to be modified
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
