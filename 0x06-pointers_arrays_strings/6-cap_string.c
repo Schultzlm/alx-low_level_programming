25c0f21a952910e611fd03d7e61470d80121889a
Schultzlm <litokwas@programmer.net>
2022-10-27 06:18:32 -0700

    Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,26 +11,25 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'
+	while (!(str[i] >= 'a' && str[i] <= 'z'))
 
-					i++;
-					if (str[i - 1] == ' ' ||
-						str[i - 1] == '\t' ||
-						str[i - 1] == '\n' ||
-						str[i - 1] == ',' ||
-						str[i - 1] == ';' ||
-						str[i - 1] == '.' ||
-						str[i - 1] == '!' ||
-						str[i - 1] == '?' ||
-						str[i - 1] == '"' ||
-						str[i - 1] == '(' ||
-						str[i - 1] == ')' ||
-						str[i - 1] == '{' ||
-						str[i - 1] == '}' ||
-						i == 0)
-					str[i] -= 32;
-
-					i++
+		i++;
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+		str[i] -= 32;
+		i++
 	}
 
 	return (str);
