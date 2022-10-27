bfb6b96e1e90dafd4da189eebca8e34f325c36b8
Schultzlm <litokwas@programmer.net>
2022-10-27 16:11:29 -0700

    Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - returns the length of a string,
  * @s: string
  * Return: returns length as integer
  */
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char transform_2(char x);
+int _strlen(char *s);
 
 #endif
