eae8cc2beb5087c70097a9dc7e0a0bc061c031b6
Schultzlm <litokwas@programmer.net>
2022-10-27 01:04:53 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,17 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	i = 0;
-	j = 0;
-
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+
+	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j]
-		j++;
+		dest[i] = src[j];
 		i++;
+		j++;
 	}
 
 	dest[i] = '\0';
