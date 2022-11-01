acc24c05d84ac9ba22e47a0a84351fe493b33294
Schultzlm <litokwas@programmer.net>
2022-11-01 06:24:39 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include  "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: first input
+ * @c: another input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
