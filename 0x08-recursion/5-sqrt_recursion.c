3e4ba624a973db97ed79efa6c8d7a644252780e0
Schultzlm <litokwas@programmer.net>
2022-11-02 08:46:58 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,41 @@
 #include "main.h"
 
+int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
 
 /**
- * _sqrt2 - evaluates fro 1 to n
+ * find_sqrt - finds the natural square root of an inputted number
  *
- * @a: same number as n
- * @b: number that iterates from 1 to n
- * Return: on success 1
- * On error, -1 is returned , and errno is set appropreately.
+ * @num: the number to find the square root of.
+ * @root: the root to be tested
+ * Return: if the number has a natural square root - the square root.
+ *         if the number does not have a natural square root --1
  */
-int _sqrt2(int a, int b)
+int find_sqrt(int num, int root)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
 		return (-1);
 
-	return (sqrt2(a, b + 1));
+	return (find_sqrt(num, root + 1));
 }
 
 /**
- * _sqrt_recursion - returns the natural square root of n
- * @n: number to calculate square root
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
  *
  * Return: On success 1
  * On error -1 is returned and errno is set appropreately
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (find_sqrt(n, root));
 }
