fa8b31e853d290a415dd43bc28467a7c306baf9a
Schultzlm <litokwas@programmer.net>
2022-11-02 15:32:47 -0700

    Inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,27 @@
+
+/*
+ * File: 6-is_prime_number.c
+ * Author: Schultz Redd
+ */
 #include "main.h"
 
-/**
- * is_prime_number - returns 1 if n is prime
- * @n: number to be checked
- *
- * Return: 1 if the n is prime, 0 otherwise
- */
-int is_prime_number(int n)
-{
-	int ssl = n / 2;
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
-	if (n < 1)
+/**
+ * is_divisible - Checks if a number is divisible
+ * @num: The number to be checked
+ * @div: the divisor
+ *
+ * Return: if the number is divisible - 0 otherwise 1
+ */
+int is divisible(int num, int div)
+{
+	if (num % div == 0)
 		return (0);
-	return (is_prime(n, ssl));
-}
 
-/**
- * is_prime - returns a 1 if n is prime
- * @n: number to be checked
- * @ssl: number to start checking from
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime(int n, int ssl)
-{
-	if (ssl < 1)
+	if (div == num / 2)
 		return (1);
-	else if (n % ssl > 0)
-		return (0);
-	return (is_prime(n, ssl - 1));
+
+	return (is_divisible(num, div + 1));
 }
