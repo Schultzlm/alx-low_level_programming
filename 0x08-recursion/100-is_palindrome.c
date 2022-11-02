3102cc7d6e1331a01146cc0c2c246c82ecf97091
Schultzlm <litokwas@programmer.net>
2022-11-02 16:50:39 -0700

    They say we only use a fraction of our brain's true potential.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,47 @@
 #include "main.h"
 
-int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
 
 /**
- * find_strlen - Returns the length of a string.
- * @s: the string to be measured.
+ * is_palindrome - checks if a string is palidrome
+ * @s: string to reverse
  *
- * Return: length of the string.
+ * Return: if palindrome 1 and 0 if not
  */
-int find_strlen(char *s)
+int is_palindrome(char *s)
 {
-	int len = 0;
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
 
-	if (*(s + len))
-	{
-		len++;
-		len += find_strlen(s + len);
-	}
+/**
+ * _strlen_recursion - returns the length of the string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
 
-	return (len);
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: if palindrome 1 otherwise 0
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
