e904c9f18fa10e10d4624e11131a0d18f56207a5
Schultzlm <litokwas@programmer.net>
2022-11-07 18:42:23 -0800

    Updated script

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+#include  "main.h"
+/**
+ * _isupper - checks for the uppercase characters.
+ * @c: The character to be checked.
+ * Return: 1 if character is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c > 'A' && c < 'Z')
+	{
+		return 1; ;;
