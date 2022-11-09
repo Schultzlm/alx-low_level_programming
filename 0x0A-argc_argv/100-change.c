43631ba79ea6b4d6702219d35bc69c427903aff2
Schultzlm <litokwas@programmer.net>
2022-11-09 11:30:01 -0800

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - entry point
+ * main - function
  * @argc: length of argv
  * @argv: number of arguments
  * Return: Always 0
@@ -15,14 +15,14 @@ int main(int argc, char *argv[])
 
 	position = total = change = aux = 0;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	total = atoi(argv[1]); /*Converts str into int*/
 
-	if (total < 0)
+	if (total <= 0)
 	{
 		printf("0\n");
 		return (0);
@@ -30,9 +30,9 @@ int main(int argc, char *argv[])
 
 	/*Declaring while*/
 
-	while (coins[position] == '\0')
+	while (coins[position] != '\0')
 	{
-	if (total > coins[position])
+	if (total >= coins[position])
 	{
 		aux = (total / coins[position]);
 		change += aux;
