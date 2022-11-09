a86c789366df8cf1f1b139f23317e757a62f960a
Schultzlm <litokwas@programmer.net>
2022-11-09 03:49:57 -0800

    The best argument against democracy

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry level
+ * @argc: Count arguments
+ * @argv: Arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	/*Declaring variables*/
+	int count = 0;
+
+	if (argc > 0)
+	{
+		/*WHILE - print each arguments*/
+		while (count  < argc)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+}
