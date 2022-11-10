01039db1c30136f08d70d388189a5ef290bbce28
Schultzlm <litokwas@programmer.net>
2022-11-10 14:11:21 -0800

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc -allocated memoria for nmeb elemn de zise bytes
+ * @nmemb: number of elementin array
+ * @size: bytes for each positionof array
+ * Return: pointer void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size;
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	p[i] = 0;
+
+	return (p);
+}
