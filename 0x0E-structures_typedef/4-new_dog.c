35faa028e781d17dc450822f5ada8b0e61b76fb1
Schultzlm <litokwas@programmer.net>
2022-11-15 14:50:49 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - returns the length of a string.
  * @s: string to evaluate
  *
  * Return: the length of the string
