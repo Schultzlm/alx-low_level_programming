483481b9d777cd5a04faef63abdd5bc8398fce91
Schultzlm <litokwas@programmer.net>
2022-11-29 08:51:46 -0800

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_listint - function that prints all the elements of a list.
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -27,7 +28,6 @@ typedef struct listp_s
 	struct listp_s *next;
 } listp_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
