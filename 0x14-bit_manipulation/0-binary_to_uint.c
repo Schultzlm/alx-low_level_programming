b4a75bc68bee466ee08ef3b9b88ab74beebdaa94
Schultzlm <litokwas@programmer.net>
2022-12-01 12:48:42 -0800

    Zer0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint - converts a binary number to an unsigned integer.
  * @b: binary
  *
  * Return: unsigned int
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This task was coded by Schultz and project name is: 0x14. C - Bit manipulation
