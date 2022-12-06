af17dc15d5dd7df3d98d772c8f3a77967314d320
Schultzlm <litokwas@programmer.net>
2022-12-05 16:16:50 -0800

    CP

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
 		if (nchars == -1)
 			error_file(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
-		if (nw == -1)
+		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
